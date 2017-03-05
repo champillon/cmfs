fc01fb7a17be7e79e5536aa8f27ccfae212e4f34
passapong.t@gmail.com <mysterior>
2017-03-05 19:10:33 +0700

    removed auto generate runnderId

--- src/main/java/th/or/operationsmile/cmfs/io/DataAccess.java
+++ src/main/java/th/or/operationsmile/cmfs/io/DataAccess.java
@@ -182,19 +182,6 @@ public class DataAccess {
 	}
 
 	public void confirmedPaySlipAndGenerateRunningKey(RegistedPerson registedPerson) throws SQLException {
-		int maxRunnerId;
-		PreparedStatement queryResultStatement = databaseConnection.prepareStatement(selectMaxRunnerID);
-		ResultSet queryResult = queryResultStatement.executeQuery();
-
-		queryResult.next();
-		maxRunnerId = queryResult.getInt("max(runnerId)");
-
-		maxRunnerId++;
-		registedPerson.setRunnerId(maxRunnerId + "");
-
-		queryResult.close();
-		queryResultStatement.close();
-
 		PreparedStatement updateResultStatement = databaseConnection.prepareStatement(updateSQLForConfirmedPayment);
 		updateResultStatement.setString(1, registedPerson.getRunnerId());
 		updateResultStatement.setInt(2, registedPerson.getRunningId());
